8df1d39614b540533e3063d39ddb01b73054f2b1
KRIPA <KRIPA@LAPTOP-20J98LRH>
2018-11-18 10:55:52 +0530

    new commit

--- src/test/MazzeTest.java
+++ src/test/MazzeTest.java
@@ -1,45 +1,58 @@
 package test;
 
 public class MazzeTest {
+	final int N = 7;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	boolean shouldContinue(String[][] maze, int x, int y) {
+
+		return (x >= 0 && x < N && y >= 0 && y < N && maze[x][y].equalsIgnoreCase("O"));
 
 	}
-	
-	public static void findPath() {
-		//goLeft();
-		goRight();
-		//goUp();
-		goDown();
-	}
-	
 
-	public static void goLeft() {
-		goLeft();
-		//goRight();
-		goUp();
-		goDown();
+	boolean findPath(String[][] maze) {
+
+		boolean flag = isAnyPath(maze, 0, 0);
+
+		if (!flag) {
+			System.out.print("No Path Found.");
+			return false;
+		}
+		return true;
 	}
 
-	public static void goRight() {
-		//goLeft();
-		goRight();
-		goUp();
-		goDown();
+	boolean isAnyPath(String[][] maze, int x, int y) {
+		if (x == (N - 1) && y == (N - 1)) {
+			if (maze[x][y].equalsIgnoreCase("X")) {
+				return false;
+			}
+			System.out.print("{" + x + ", " + y + "}");
+			return true;
+		}
+
+		if (shouldContinue(maze, x, y)) {
+
+			System.out.print("{" + x + ", " + y + "} -> ");
+
+			if (isAnyPath(maze, x + 1, y)) {
+				return true;
+			}
+
+			if (isAnyPath(maze, x, y + 1)) {
+				return true;
+			}
+
+			return false;
+		}
+
+		return false;
 	}
 
-	public static void goUp() {
-		goLeft();
-		goRight();
-		goUp();
-		//goDown();
-	}
-
-	public static void goDown() {
-		goLeft();
-		goRight();
-		//goUp();
-		goDown();
+	public static void main(String args[]) {
+		MazzeTest test = new MazzeTest();
+		String maze[][] = { { "O", "O", "O", "O", "O", "X", "O" }, { "X", "X", "O", "X", "O", "O", "X" },
+				{ "O", "X", "O", "O", "X", "X", "X" }, { "X", "X", "X", "O", "O", "X", "O" },
+				{ "X", "X", "X", "X", "O", "O", "X" }, { "O", "O", "O", "O", "O", "O", "O" },
+				{ "X", "X", "O", "X", "X", "X", "O" } };
+		test.findPath(maze);
 	}
 }
